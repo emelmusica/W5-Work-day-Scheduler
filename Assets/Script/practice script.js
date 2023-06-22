a236a80906e86f576658d1a94cbc2b9749972f04
Em3L <martink_lopez@yahoo.com>
2023-06-22 17:56:52 +1000

    changing some styling

--- Assets/Script/practice script.js
+++ Assets/Script/practice script.js
@@ -30,16 +30,19 @@ $(document).ready(function() {
         // Additional condition to make 1pm to 5pm blocks green
         if (hour >= 13 && hour <= 17) {
           descriptionColumn.removeClass("past present future").addClass("future");
-        }  
+        }
   
         // Retrieve saved event from local storage
         var savedEvent = localStorage.getItem("event_" + hour);
         if (savedEvent) {
           descriptionColumn.val(savedEvent);
+          if (hour === currentHour) {
+            $("<div>").addClass("appointment-added").text("Appointment Added to localStorage").insertAfter(hourColumn);
+          }
         }
   
         // Event listener for saving the event to local storage
-        saveButton.on("click", function () {
+        saveButton.on("click", function() {
           var event = $(this).siblings(".description").val();
           var hour = $(this).parent().attr("data-hour");
           localStorage.setItem("event_" + hour, event);
