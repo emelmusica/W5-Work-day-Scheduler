47ff6a830089e02406ba03f50dadb78c9a8fbe2a
Em3L <martink_lopez@yahoo.com>
2023-06-26 13:25:07 +1000

    updated javascript to reflect colours for p,f,pres

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,44 +1,40 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // Add a listener for click events on the save button.
-  $(".saveBtn").on("click", function () {
-    // Get the id of the containing time-block.
-    var timeBlockId = $(this).closest(".time-block").attr("id");
+$(document).ready(function () {
+  // Display current date
+  var currentDate = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(currentDate);
 
-    // Get the user input from the description textarea.
-    var userInput = $(this).siblings(".description").val();
-
-    // Save the user input in local storage using the time block id as the key.
-    localStorage.setItem(timeBlockId, userInput);
-  });
-
+  // Apply the past, present, or future class to each time block by comparing the id to the current hour.
   var currentHour = dayjs().hour();
-$(".container-fluid.px-5 .time-block").each(function () {
-  var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+  $(".container-fluid .time-block").each(function () {
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
 
-  if (timeBlockHour < currentHour) {
-    $(this).removeClass("present future").addClass("past");
-  } else if (timeBlockHour === currentHour) {
-    $(this).removeClass("past future").addClass("present");
-  } else {
-    $(this).removeClass("past present").addClass("future");
-  }
-});
-
-  
-  // Get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
-  $(".time-block").each(function () {
-    var timeBlockId = $(this).attr("id");
-    var userInput = localStorage.getItem(timeBlockId);
-
-    if (userInput) {
-      $(this).find(".description").val(userInput);
+    if (timeBlockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
     }
   });
 
-  // Display the current date in the header of the page.
-  var currentDate = dayjs().format("dddd, MMMM DD");
-  $("#currentDay").text(currentDate);
+  // Save user input to local storage
+  $(".saveBtn").on("click", function () {
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+    var userEvent = $(this).siblings(".description").val().trim();
+
+    localStorage.setItem(timeBlockId, userEvent);
+  });
+
+  // Load saved events from local storage
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var savedEvent = localStorage.getItem(timeBlockId);
+
+    if (savedEvent) {
+      $(this).find(".description").val(savedEvent);
+    }
+  });
 });
